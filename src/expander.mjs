315ba8cd9aa2818649aa402f8f4fc990d42f04dd
Markus Felten <Markus.Felten@gmx.de>
2024-03-11 21:06:21 +0100

    fix: optional 2nd. param

--- src/expander.mjs
+++ src/expander.mjs
@@ -24,7 +24,7 @@ function _quote(str) {
 /**
  * @typedef {Object} ExpressionExpander
  * @property {Object} properties
- * @property {function(string|boolean|number|bigint|Object|Map|Set,[PathEntry[]]):any} expand
+ * @property {function(string|boolean|number|bigint|Object|Map|Set,PathEntry[]?):any} expand
  */
 
 /**
